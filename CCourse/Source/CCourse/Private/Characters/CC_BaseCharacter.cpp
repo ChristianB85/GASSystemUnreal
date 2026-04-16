6dfd9c103e8b9bde12c725c647b3dfdbb1d07e6f
Chris <165912142+ChristianB85@users.noreply.github.com>
2026-04-16 23:12:28 +0200

    Initial commit

---
+++ CCourse/Source/CCourse/Private/Characters/CC_BaseCharacter.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CCourse/Public/Characters/CC_BaseCharacter.h"
+
+
+// Sets default values
+ACC_BaseCharacter::ACC_BaseCharacter()
+{
+
+	PrimaryActorTick.bCanEverTick = false;
+	
+	//Tick and refresh bone transforms on a dedicated server - whether they are rendered or not 
+	GetMesh()->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::AlwaysTickPoseAndRefreshBones;
+}
+
+
+
+
+
---
+++ CCourse/Source/CCourse/Public/Characters/CC_PlayerCharacter.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CC_BaseCharacter.h"
+#include "CC_PlayerCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class CCOURSE_API ACC_PlayerCharacter : public ACC_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACC_PlayerCharacter();
+
+
+private:
+	UPROPERTY(VisibleAnywhere, Category="Camera")
+	TObjectPtr<USpringArmComponent> CameraBoom;
+	
+	UPROPERTY(VisibleAnywhere, Category="Camera")
+	TObjectPtr<UCameraComponent> FollowCamera;
+};
